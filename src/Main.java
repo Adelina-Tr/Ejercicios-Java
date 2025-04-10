5c67c7a9c0f105390bb9d6f12a71aef874b9da29
Adelina Trejo <kf8@hotmail.com>
2025-04-09 22:15:33 -0600

    text block

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("bienvenido(a) a ScreenMatch");
+        System.out.println("Bienvenido(a) a ScreenMatch");
+        System.out.println("Película: Matrix");
+        int FechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+
+        double notaDeLaPelicula = 8.2;
+        double media = (8.2 + 6.0 + 9.0)/3;
+        System.out.println(media);
+
+        String sinopsis = """
+                Matrix es una pradoja
+                La mejor pelicula del fin del milenio
+                Fue lanzada en:
+                """+FechaDeLanzamiento;
+        System.out.println(sinopsis);
     }
 }
