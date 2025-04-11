b3ce22903f9a4ed35e1f2f07c8e89a16ef21e8b2
Adelina Trejo <kf8@hotmail.com>
2025-04-11 10:27:38 -0600

    Loops

---
+++ src/Programa.java
@@ -0,0 +1,18 @@
+public class Programa {
+
+    //Ejercicio: Convertir este ciclo while en un for
+
+    public static void main(String[] args) {
+
+//        int contador = 1;
+//
+//        while(contador <= 10) {
+//            System.out.println(contador);
+//            contador++;
+//        }
+
+        for(int i= 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
